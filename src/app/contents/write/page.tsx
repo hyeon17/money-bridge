375bf21d5b5fd341c83ffb107d8c4165bfc3e71d
hyeon17 <janghyeon8725@naver.com>
2023-06-21 08:07:32 +0900

    Feat: 콘텐츠 페이지, 컴포넌트 추가

--- src/app/contents/write/page.tsx
+++ src/app/contents/write/page.tsx
@@ -1,9 +1,16 @@
+import TopNav from '@/components/common/TopNav'
+import Write from '@/components/contentsPage/Write'
 import React from 'react'
+import { pbWriteContent } from '@/app/apis/services/pb';
+import { useMutation } from 'react-query';
 
 function ContentsWrite() {
   return (
-    <div>ContentsWrite</div>
-  )
+    <>
+      <TopNav title="콘텐츠 작성하기" hasBack={true} />
+      <Write />
+    </>
+  );
 }
 
 export default ContentsWrite
--- src/components/contentsPage/Poster.tsx
+++ src/components/contentsPage/Poster.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
 
-function Poster({ img }: any) {
-  return <Image src={img} alt="poster" width={300} height={300} />;
+function Poster({img} :{img:any}) {
+  return <Image src={img} alt="poster" width={0} height={390} className="h-[390px] w-auto mx-auto" priority={true} />;
 }
 
 export default Poster;
